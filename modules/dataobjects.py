16f393f4b8b2a2befbdd03bc8f7e7d602e319a07
Steven Case <steveacase96@gmail.com>
2026-04-09 18:39:02 -0500

    version update

--- modules/dataobjects.py
+++ modules/dataobjects.py
@@ -100,8 +100,8 @@ class AppSettings:
 
     streamer_name: str = "mycelian"
     streamer_id: str = ""
-    version: str = "1.6.0"
-    build_date: str = "March 21st 2026"
+    version: str = "1.7.0"
+    build_date: str = "April 9th 2026"
     alert_volume: float = 0.5
     auto_reconnect: bool = True
     current_theme: str = "dark"
